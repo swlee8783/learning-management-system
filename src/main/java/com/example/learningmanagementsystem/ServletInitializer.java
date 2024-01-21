032713119516e45b54c86b23245f387c37754b25
swlee8783 <siwon8783@gmail.com>
2024-01-21 15:09:52 +0900

    feat: add date for update

--- src/main/java/com/example/learningmanagementsystem/ServletInitializer.java
+++ src/main/java/com/example/learningmanagementsystem/ServletInitializer.java
@@ -2,9 +2,9 @@ package com.example.learningmanagementsystem;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    //2024.01.21
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(LearningManagementSystemApplication.class);
